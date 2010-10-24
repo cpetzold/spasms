3ce48f86a744cbded9f2ff447f5c5023cda2c448
Brendan Nee <brendan@blinktag.com>
2010-10-24 16:56:21 -0700

    Updated signup link to have signup form

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,15 @@ var message = function(txt){
 var socket = new io.Socket(null, {port: 80});
 socket.connect();
 socket.on('message', function(obj){
+
   console.log(obj);
 });
 
 socket.send('some data');
+
+$(document).ready(function()
+   {
+     $('#signup').click(function(){
+       $('#signup-form').toggle();
+     });
+});

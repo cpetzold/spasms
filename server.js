b9707cfbbe206b3ae0f686f9a59f9b305423e8b5
Nathan White <nw@nwhite.net>
2010-10-24 15:55:35 -0700

    added socket.io to server

--- server.js
+++ server.js
@@ -37,3 +37,23 @@ app.get('/sms', function(req, res){
 var port = 80;
 console.log("Game on! (port: " + port + ")");
 if (!module.parent) app.listen(port);
+
+var io = require('socket.io').listen(app);
+var buffer = []; 
+
+io.on('connection', function(client){
+  client.send({ buffer: buffer });
+  client.broadcast({ announcement: client.sessionId + ' connected' });
+  
+  client.on('message', function(message){
+    var msg = { message: [client.sessionId, message ]};
+    buffer.push(msg);
+    if(buffer.length > 15) buffer.shift();
+    clent.broadcast(msg);
+  });
+  
+  client.on('disconnect', function(){
+    client.broadcast({ announcement: client.sessionId + ' disconnected' });
+  })
+  
+});

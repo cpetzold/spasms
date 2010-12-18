0ec67714a480d8111d9016a36eadcb22d08fc532
Conner <cpetzold@gmail.com>
2010-12-17 20:07:26 -0800

    started wit styling and client-side code

--- public/js/spasms.js
+++ public/js/spasms.js
@@ -1,24 +1,39 @@
 (function($) {
   
 function Spasms(p) {
-  this.socket = new io.Socket('localhost', {port: p});
+  this.socket = new io.Socket(null, {port: p});
   this.socket.connect();
   
   this.el = {
-    join: $('#join'),
-    form: $('#form'),
-    name: $('#name'),
-    phone: $('#phone'),
-    goplay: $('#goplay')
+    output: $('#output'),
+    input: $('#input'),
+    scroll: $('#scroll')
   };
   
+  var self = this;
   this.socket.on('message', function(actions){
     $.each(actions, function(action, o){
       console.log(action, o);
+      self[action](o);
     });
-  });
+  }); 
   
+  
+  this.el.input.focus();
 }
+
+Spasms.prototype = {
+  roundStart: function(o) {
+    // var msg = $('<p>').html(o.string);
+    // this.el.scroll.append(msg);
+    
+  },
+  roundEnd: function(o) {
+    
+  }
+  
+  
+};
 
 var app = new Spasms(80);
 
